c263f8afac5d4822bb4a9c5d8ff642b88d5e0c11
MadalynBotkin <mbotkin@uccs.edu>
2022-03-28 21:46:20 +0000

    Replaced the word Project with Task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,21 +1,21 @@
 #unit tests for the Task model
 require "rails_helper"
 
-RSpec.describe Project, type: :model do
+RSpec.describe Task, type: :model do
   context "validations tests" do
     it "ensures the title is present" do
-      project = Project.new(description: "Content of the description")
-      expect(project.valid?).to eq(false)
+      task = Task.new(description: "Content of the description")
+      expect(task.valid?).to eq(false)
     end
 
     it "ensures the description is present" do
-      project = Project.new(title: "Title")
-      expect(project.valid?).to eq(false)
+      task = Task.new(title: "Title")
+      expect(task.valid?).to eq(false)
     end
     
-    it "should be able to save project" do
-      project = Project.new(title: "Title", description: "Some description content goes here")
-      expect(project.save).to eq(true)
+    it "should be able to save task" do
+      task = Task.new(title: "Title", description: "Some description content goes here")
+      expect(task.save).to eq(true)
     end
   end
 
