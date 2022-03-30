94f2b7b71eaf7f909ed7aaf7438d265153038002
MadalynBotkin <mbotkin@uccs.edu>
2022-03-30 22:52:05 +0000

    Commented out SimpleCov code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,9 +63,11 @@ RSpec.configure do |config|
 end
 
 #SimpleCov
+=begin
 require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
   add_filter '/spec/' # for rspec
 end
+=end
